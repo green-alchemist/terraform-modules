c98314834565bf2d524ace573812298d9fe6c8ce
sigma-us <kyleconley2@gmail.com>
2023-01-31 14:04:09 -0800

    variable documentation

--- modules/ecr/variables.tf
+++ modules/ecr/variables.tf
@@ -3,9 +3,24 @@ variable "tags" {
   type        = map(string)
   default     = {}
 }
+## tags example
+  # tags = {
+  #   Terraform = "true"
+  # }
 
 variable "ecrs" {
   description = "Map of ECRs to create."
   type        = any
   default     = {}
 }
+## ercs example
+  # ecrs = {
+  #   strapi-admin-dev = {
+  #     tags = { Service = "strapi-admin-dev", Env = "dev" }
+  #     lifecycle_policy = local.lifecycle_policy # use if overriding default policy
+  #   },
+  #   strapi-admin-prod = {
+  #     tags = { Service = "strapi-admin-prod", Env = "prod" }
+  #   }
+  # }
+  
