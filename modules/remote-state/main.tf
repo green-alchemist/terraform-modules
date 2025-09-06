53fc5456ff1b4eaeee2b2609e453c0c2eea98261
green-alchemist <kyleconley2@gmail.com>
2025-09-05 18:02:20 -0700

    remote-state

--- modules/remote-state/main.tf
+++ modules/remote-state/main.tf
@@ -103,9 +103,15 @@ resource "aws_s3_bucket_policy" "s3_policy" {
   depends_on = [aws_s3_bucket_public_access_block.state]
 }
 
-resource "aws_s3_bucket_acl" "state" {
+# resource "aws_s3_bucket_acl" "state" {
+#   bucket = aws_s3_bucket.state.id
+#   acl    = "private"
+# }
+resource "aws_s3_bucket_ownership_controls" "state" {
   bucket = aws_s3_bucket.state.id
-  acl    = "private"
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
 }
 
 resource "aws_s3_bucket_versioning" "state" {
