7f8a049b3a926db2992a81a57f2808f93c6e06fc
green-alchemist <kyleconley2@gmail.com>
2025-09-16 16:30:27 -0700

    task fmt

--- modules/aurora-serverless/main.tf
+++ modules/aurora-serverless/main.tf
@@ -4,16 +4,16 @@ resource "aws_db_subnet_group" "this" {
 }
 
 resource "aws_rds_cluster" "this" {
-  cluster_identifier      = var.database_name
-  engine                  = "aurora-postgresql"
-  engine_mode             = "provisioned"
-  engine_version          = "13.7"
-  database_name           = var.database_name
-  master_username         = var.master_username
-  master_password         = var.master_password
-  db_subnet_group_name    = aws_db_subnet_group.this.name
-  vpc_security_group_ids  = var.security_group_ids
-  skip_final_snapshot     = true
+  cluster_identifier     = var.database_name
+  engine                 = "aurora-postgresql"
+  engine_mode            = "provisioned"
+  engine_version         = "13.7"
+  database_name          = var.database_name
+  master_username        = var.master_username
+  master_password        = var.master_password
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+  vpc_security_group_ids = var.security_group_ids
+  skip_final_snapshot    = true
 
   serverlessv2_scaling_configuration {
     min_capacity = 0.5
