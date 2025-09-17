d3ac87070783f425ab37f0e936983c2f88de28c5
green-alchemist <kyleconley2@gmail.com>
2025-09-16 18:57:27 -0700

    add hosted zone id output

--- modules/cloudfront/outputs.tf
+++ modules/cloudfront/outputs.tf
@@ -7,3 +7,8 @@ output "domain_name" {
   description = "The domain name of the CloudFront distribution."
   value       = aws_cloudfront_distribution.this.domain_name
 }
+
+output "hosted_zone_id" {
+  description = "The hosted zone ID of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.this.hosted_zone_id
+}
