bed183e5f308321a8a64fd0b89f7ef962b6284d7
green-alchemist <kyleconley2@gmail.com>
2025-09-16 17:27:41 -0700

    fix??

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -9,10 +9,10 @@ resource "aws_security_group" "this" {
       from_port = ingress.value.from_port
       to_port   = ingress.value.to_port
       protocol  = ingress.value.protocol
-      # Use try() to gracefully handle rules that don't define cidr_blocks
-      cidr_blocks = try(ingress.value.cidr_blocks, null)
-      # Use try() to gracefully handle rules that don't define security_groups
-      security_groups = try(ingress.value.security_groups, null)
+      # Safely look up the attributes. If not found, the resulting null
+      # value will cause Terraform to omit the argument entirely.
+      cidr_blocks     = lookup(ingress.value, "cidr_blocks", null)
+      security_groups = lookup(ingress.value, "security_groups", null)
     }
   }
 
@@ -22,8 +22,8 @@ resource "aws_security_group" "this" {
       from_port       = egress.value.from_port
       to_port         = egress.value.to_port
       protocol        = egress.value.protocol
-      cidr_blocks     = try(egress.value.cidr_blocks, null)
-      security_groups = try(egress.value.security_groups, null)
+      cidr_blocks     = lookup(egress.value, "cidr_blocks", null)
+      security_groups = lookup(egress.value, "security_groups", null)
     }
   }
 }
