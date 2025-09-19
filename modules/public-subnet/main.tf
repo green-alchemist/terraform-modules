b0861c326714a7d2688919e11071879a61d90036
green-alchemist <kyleconley2@gmail.com>
2025-09-19 14:30:38 -0700

    conflicting count

--- modules/public-subnet/main.tf
+++ modules/public-subnet/main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public_subnet" {
+  # count                   = var.subnet_should_be_created ? 1 : 0
   for_each                = var.public_subnets
-  count                   = var.subnet_should_be_created ? 1 : 0
   vpc_id                  = var.vpc_id
   cidr_block              = each.value
   availability_zone       = each.key
