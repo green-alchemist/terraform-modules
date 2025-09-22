3d1bf2ca775dc65c9d2b08308e34702c63f2da99
green-alchemist <kyleconley2@gmail.com>
2025-09-21 18:08:40 -0700

    comments ?

--- modules/ecs-task-execution-role/main.tf
+++ modules/ecs-task-execution-role/main.tf
@@ -1,43 +1,37 @@
 resource "aws_iam_role" "this" {
   name = var.role_name
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = { Service = "ecs-tasks.amazonaws.com" }
       }
     ]
   })
 }
 
-# Define the specific permissions needed
 data "aws_iam_policy_document" "ecs_task_policy" {
   statement {
     effect = "Allow"
     actions = [
+      "ecr:GetAuthorizationToken",
       "ecr:BatchCheckLayerAvailability",
       "ecr:GetDownloadUrlForLayer",
       "ecr:BatchGetImage",
-      "ecr:GetAuthorizationToken",
       "logs:CreateLogStream",
       "logs:PutLogEvents"
     ]
-    resources = ["*"] # Note: For production, you could scope these down further
+    resources = ["*"]
   }
 }
 
-# Create a policy from the definition above
 resource "aws_iam_policy" "this" {
   name   = "${var.role_name}-policy"
   policy = data.aws_iam_policy_document.ecs_task_policy.json
 }
 
-# Attach the new, specific policy to the role
 resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.this.arn
