5714e06a8fe6f11f2ac9c9b478c6b89fa9b73630
green-alchemist <kyleconley2@gmail.com>
2025-09-24 21:34:16 -0700

    task role arn?

--- modules/fargate/main.tf
+++ modules/fargate/main.tf
@@ -9,6 +9,7 @@ resource "aws_ecs_task_definition" "this" {
   cpu                      = var.task_cpu
   memory                   = var.task_memory
   execution_role_arn       = var.ecs_task_execution_role_arn
+  task_role_arn            = var.ecs_task_execution_role_arn
 
   container_definitions = jsonencode([
     {
