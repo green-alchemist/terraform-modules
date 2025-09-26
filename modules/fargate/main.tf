f4bec21807693b0c2437a0962b9c4acd0d73f9d2
green-alchemist <kyleconley2@gmail.com>
2025-09-25 23:26:08 -0700

    okko

--- modules/fargate/main.tf
+++ modules/fargate/main.tf
@@ -84,7 +84,8 @@ resource "aws_ecs_service" "this" {
     for_each = var.enable_service_discovery ? [1] : []
     content {
       registry_arn = aws_service_discovery_service.this[0].arn
-      container_port = 1337
+      container_name = var.container_name
+      container_port = var.container_port
     }
   }
 
