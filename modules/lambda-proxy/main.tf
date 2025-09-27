1e3ec316b8f9ae3486a2dc357f18160e4e999a10
green-alchemist <kyleconley2@gmail.com>
2025-09-27 00:42:37 -0700

    depends

--- modules/lambda-proxy/main.tf
+++ modules/lambda-proxy/main.tf
@@ -340,6 +340,7 @@ resource "aws_security_group" "lambda" {
       Name = "${var.project_prefix}-${var.service_name}-lambda-proxy"
     }
   )
+  depends_on = [aws_lambda_function.proxy]
 }
 
 # Egress rule for target service
