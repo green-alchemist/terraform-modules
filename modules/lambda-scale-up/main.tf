733418e36c9c70bf61da4fbece74da3e5e6f7feb
green-alchemist <kyleconley2@gmail.com>
2025-10-01 19:52:17 -0700

    ok

--- modules/lambda-scale-up/main.tf
+++ modules/lambda-scale-up/main.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "scale_trigger" {
-  filename         = data.archive_file.lambda_zip.output_path
+  filename = data.archive_file.lambda_zip.output_path
   # source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  function_name    = "${var.service_name}-scale-trigger"
-  role             = aws_iam_role.lambda.arn
-  handler          = "index.handler"
-  runtime          = "nodejs20.x"
-  timeout          = 60
+  function_name = "${var.service_name}-scale-trigger"
+  role          = aws_iam_role.lambda.arn
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+  timeout       = 60
 
   environment {
     variables = {
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "scale_trigger" {
   }
 
   lifecycle {
-    ignore_changes = [last_modified]  # Ignore last_modified to fix provider bug
+    ignore_changes = [last_modified] # Ignore last_modified to fix provider bug
   }
 }
 
