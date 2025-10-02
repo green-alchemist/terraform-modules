2488d1e43874c512849b53e996c84f4e2dd362b9
green-alchemist <kyleconley2@gmail.com>
2025-10-01 23:33:02 -0700

    modify scaling

--- modules/fargate/main.tf
+++ modules/fargate/main.tf
@@ -123,13 +123,25 @@ resource "aws_appautoscaling_policy" "scale" {
   scalable_dimension = aws_appautoscaling_target.this[0].scalable_dimension
   service_namespace  = aws_appautoscaling_target.this[0].service_namespace
 
+  # target_tracking_scaling_policy_configuration {
+  #   target_value = var.cpu_utilization_low_threshold # New variable
+  #   predefined_metric_specification {
+  #     predefined_metric_type = "ECSServiceAverageCPUUtilization" # Valid metric
+  #   }
+  #   scale_in_cooldown     = var.scale_in_cooldown  # 5 min to prevent rapid scale-down
+  #   scale_out_cooldown    = var.scale_out_cooldown # Fast scale-up from zero
+  # }
   target_tracking_scaling_policy_configuration {
-    target_value = var.cpu_utilization_low_threshold # New variable
-    predefined_metric_specification {
-      predefined_metric_type = "ECSServiceAverageCPUUtilization" # Valid metric
-    }
-    scale_in_cooldown     = var.scale_in_cooldown  # 5 min to prevent rapid scale-down
-    scale_out_cooldown    = var.scale_out_cooldown # Fast scale-up from zero
+  target_value = 0.01  # e.g., 10% of max requests
+  customized_metric_specification {
+    metric_name = "RequestCount"
+    namespace   = "YourApp/Strapi"
+    statistic   = "Sum"
+    unit        = "Count"
+    dimensions = [
+      { name = "ServiceName", value = var.service_name },
+      { name = "ClusterName", value = var.cluster_name }
+    ]
   }
 }
 
