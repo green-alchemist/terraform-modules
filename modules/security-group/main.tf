e99ba1f7922f9169f7ac0f8e03d37f7c21fcbb8e
green-alchemist <kyleconley2@gmail.com>
2025-10-02 15:54:44 -0700

    self this

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -9,6 +9,7 @@ resource "aws_security_group" "this" {
       from_port = ingress.value.from_port
       to_port   = ingress.value.to_port
       protocol  = ingress.value.protocol
+      self      = ingress.value.self
       # Safely look up the attributes. If not found, the resulting null
       # value will cause Terraform to omit the argument entirely.
       cidr_blocks     = lookup(ingress.value, "cidr_blocks", null)
@@ -19,9 +20,11 @@ resource "aws_security_group" "this" {
   dynamic "egress" {
     for_each = var.egress_rules
     content {
-      from_port       = egress.value.from_port
-      to_port         = egress.value.to_port
-      protocol        = egress.value.protocol
+      from_port = egress.value.from_port
+      to_port   = egress.value.to_port
+      protocol  = egress.value.protocol
+      self      = ingress.value.self
+
       cidr_blocks     = lookup(egress.value, "cidr_blocks", null)
       security_groups = lookup(egress.value, "security_groups", null)
     }
