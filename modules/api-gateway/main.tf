06930440b509a19f87af4d829c6f24cc3d2d8ddc
green-alchemist <kyleconley2@gmail.com>
2025-10-08 15:40:56 -0700

    nope

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -83,7 +83,7 @@ resource "aws_apigatewayv2_integration" "this" {
   credentials_arn        = var.enable_lambda_proxy ? aws_iam_role.api_gateway_sfn_role[0].arn : null
 
   request_parameters = var.enable_lambda_proxy ? {
-    "Input"           = "$request"
+    "Input"           = "$$request"
     "StateMachineArn" = one(module.step_function[*].state_machine_arn)
   } : {}
 }
