3c835380312c0ffc821bdc6127582e8bca9793e1
green-alchemist <kyleconley2@gmail.com>
2025-10-07 17:45:48 -0700

    ok

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -82,9 +82,14 @@ resource "aws_apigatewayv2_integration" "this" {
 
   credentials_arn = var.enable_lambda_proxy ? aws_iam_role.api_gateway_sfn_role[0].arn : null
 
-  request_parameters = var.enable_lambda_proxy ? {
-    "Input"                                   = "$context.request.input"
-    "StateMachineArn"                         = "${one(module.step_function[*].state_machine_arn)}"
+ request_templates = var.enable_lambda_proxy ? {
+    # This template will be used for all requests
+    "application/json" = <<-EOF
+    {
+      "input": "$util.escapeJavaScript($input.json('$'))",
+      "stateMachineArn": "${one(module.step_function[*].state_machine_arn)}"
+    }
+    EOF
   } : null
 
   depends_on = [aws_iam_role_policy.api_gateway_sfn_policy]
