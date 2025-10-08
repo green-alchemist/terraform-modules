1ffc7ddeaa22e117d5af9e8b1b5e9c9c1ffa9ffa
green-alchemist <kyleconley2@gmail.com>
2025-10-08 12:43:18 -0700

    aws manual

--- modules/api-gateway/main.tf
+++ modules/api-gateway/main.tf
@@ -71,7 +71,7 @@ resource "aws_apigatewayv2_api" "this" {
 # Creates the integration that connects the API to the backend (conditional on mode)
 resource "aws_apigatewayv2_integration" "this" {
   api_id              = aws_apigatewayv2_api.this.id
-  integration_type    = var.enable_lambda_proxy ? "AWS_PROXY" : var.integration_type
+  integration_type    = var.enable_lambda_proxy ? "AWS" : var.integration_type
   integration_subtype = var.enable_lambda_proxy ? "StepFunctions-StartSyncExecution" : null
   integration_method  = var.enable_lambda_proxy ? null : var.integration_method
   integration_uri     = var.enable_lambda_proxy ? null : var.integration_uri
