808d97e18dd1ca9c2c65489ff6ce2242aa3c8eda
green-alchemist <kyleconley2@gmail.com>
2025-10-08 19:52:39 -0700

    resource name!!

--- modules/lambda-wake-proxy/outputs.tf
+++ modules/lambda-wake-proxy/outputs.tf
@@ -1,14 +1,14 @@
 output "lambda_invoke_arn" {
-  value       = aws_lambda_function.scale_trigger.invoke_arn
+  value       = aws_lambda_function.wake_proxy.invoke_arn
   description = "The invoke ARN of the Lambda function."
 }
 
 output "lambda_arn" {
-  value       = aws_lambda_function.scale_trigger.arn
+  value       = aws_lambda_function.wake_proxy.arn
   description = "The ARN of the Lambda function."
 }
 
 output "lambda_function_name" {
-  value       = aws_lambda_function.scale_trigger.function_name
+  value       = aws_lambda_function.wake_proxy.function_name
   description = "The name of the Lambda function."
 }
