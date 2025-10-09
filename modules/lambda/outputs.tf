d0bdec7b7d957c4402449636603cc9bedfb4a14e
green-alchemist <kyleconley2@gmail.com>
2025-10-08 21:22:54 -0700

    outputs oops this

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -1,20 +1,20 @@
 output "lambda_invoke_arns" {
   value = {
-    for name, lambda in aws_lambda_function.wake_proxy : name => lambda.invoke_arn
+    for name, lambda in aws_lambda_function.this : name => lambda.invoke_arn
   }
   description = "Map of Lambda invoke ARNs, keyed by Lambda configuration name."
 }
 
 output "lambda_arns" {
   value = {
-    for name, lambda in aws_lambda_function.wake_proxy : name => lambda.arn
+    for name, lambda in aws_lambda_function.this : name => lambda.arn
   }
   description = "Map of Lambda ARNs, keyed by Lambda configuration name."
 }
 
 output "lambda_function_names" {
   value = {
-    for name, lambda in aws_lambda_function.wake_proxy : name => lambda.function_name
+    for name, lambda in aws_lambda_function.this : name => lambda.function_name
   }
   description = "Map of Lambda function names, keyed by Lambda configuration name."
 }
