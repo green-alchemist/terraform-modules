195480bad7f9ab63be6e58f3e876abceed68288c
green-alchemist <kyleconley2@gmail.com>
2025-10-09 18:00:59 -0700

    hm

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -26,7 +26,7 @@ resource "aws_lambda_layer_version" "python_packages" {
   filename            = "${path.module}/lambda/${each.key}-layer.zip"
   source_code_hash    = filebase64sha256("${path.module}/lambda/${each.key}-layer.zip")
 
-  depends_on = [null_resource.python_package_layer[each.key]]
+  depends_on = [null_resource.python_package_layer]
 }
 
 # Create a ZIP file in memory for each Lambda
